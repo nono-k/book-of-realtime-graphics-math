60a3750997e39a673d3784396e4d6ed3181b9ce9
iwasaki <poettype73@gmail.com>
2025-09-04 00:04:39 +0900

    Add posterization02

---
+++ src/shaders/p025_posterization02.glsl
@@ -0,0 +1,27 @@
+precision mediump float;
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+varying vec2 vUv;
+
+void main() {
+  vec2 pos = vUv;
+
+  vec3[4] col4 = vec3[](
+    vec3(1.0, 0.0, 0.0),
+    vec3(1.0, 1.0, 0.0),
+    vec3(1.0, 0.0, 1.0),
+    vec3(1.0, 1.0, 1.0)
+  );
+
+  float n = 4.0;
+  pos *= n;
+  float thr = 0.25 * sin(u_time);
+  pos = floor(pos) + smoothstep(0.25 + thr, 0.75 - thr, fract(pos));
+  pos /= n;
+
+  vec3 col = mix(mix(col4[0], col4[1], pos.x), mix(col4[2], col4[3], pos.x), pos.y);
+
+  gl_FragColor = vec4(col, 1.0);
+}
