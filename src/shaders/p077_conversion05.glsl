9e97e47ba8e9943ff58f739240a6b475f5e6941c
iwasaki <poettype73@gmail.com>
2025-10-07 00:26:14 +0900

    Add conversion05

---
+++ src/shaders/p077_conversion05.glsl
@@ -0,0 +1,107 @@
+precision mediump float;
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+varying vec2 vUv;
+
+const float PI = 3.1415926;
+
+uvec3 k = uvec3(0x456789abu, 0x6789ab45u, 0x89ab4567u);
+uvec3 u = uvec3(1, 2, 3);
+const uint UINT_MAX = 0xffffffffu;
+
+uint uhash11(uint n){
+  n ^= (n << u.x);
+  n ^= (n >> u.x);
+  n *= k.x;
+  n ^= (n << u.x);
+  return n * k.x;
+}
+
+uvec2 uhash22(uvec2 n){
+  n ^= (n.yx << u.xy);
+  n ^= (n.yx >> u.xy);
+  n *= k.xy;
+  n ^= (n.yx << u.xy);
+  return n * k.xy;
+}
+
+uvec3 uhash33(uvec3 n){
+  n ^= (n.yzx << u);
+  n ^= (n.yzx >> u);
+  n *= k;
+  n ^= (n.yzx << u);
+  return n * k;
+}
+
+float hash11(float p){
+  uint n = floatBitsToUint(p);
+  return float(uhash11(n)) / float(UINT_MAX);
+}
+
+float hash21(vec2 p){
+  uvec2 n = floatBitsToUint(p);
+  return float(uhash22(n).x) / float(UINT_MAX);
+}
+
+float hash31(vec3 p){
+  uvec3 n = floatBitsToUint(p);
+  return float(uhash33(n).x) / float(UINT_MAX);
+}
+
+vec2 hash22(vec2 p){
+  uvec2 n = floatBitsToUint(p);
+  return vec2(uhash22(n)) / vec2(UINT_MAX);
+}
+
+vec3 hash33(vec3 p){
+  uvec3 n = floatBitsToUint(p);
+  return vec3(uhash33(n)) / vec3(UINT_MAX);
+}
+
+float vnoise21(vec2 p){
+  vec2 n = floor(p);
+  float[4] v;
+  for (int j = 0; j < 2; j ++){
+    for (int i = 0; i < 2; i++){
+      v[i+2*j] = hash21(n + vec2(i, j));
+    }
+  }
+  vec2 f = fract(p);
+  f = f * f * f * (10.0 - 15.0 * f + 6.0 * f * f);
+  return mix(mix(v[0], v[1], f[0]), mix(v[2], v[3], f[0]), f[1]);
+}
+
+float fbm21(vec2 p, float g) {
+  float val = 0.0;
+  float amp = 1.0;
+  float freq = 1.0;
+
+  for (int i = 0; i < 4; i++) {
+    val += amp * (vnoise21(freq * p) - 0.5);
+    amp *= g;
+    freq *= 2.01;
+  }
+  return 0.5 * val + 0.5;
+}
+
+float warp21(vec2 p, float g) {
+  float val = 0.0;
+  for (int i = 0; i < 4; i++) {
+    val = fbm21(p + g * vec2(cos(2.0 * PI * val), sin(2.0 * PI * val)), 0.5);
+  }
+  return val;
+}
+
+float converter(float v) {
+  return 0.5 * sin(4.0 * PI * v + u_time) + 0.5;
+}
+
+void main() {
+  vec2 pos = gl_FragCoord.xy / min(u_resolution.x, u_resolution.y);
+  pos = 10.0 * pos + u_time;
+  vec3 col = vec3(converter(warp21(pos, 1.0)));
+
+  gl_FragColor = vec4(col, 1.0);
+}
